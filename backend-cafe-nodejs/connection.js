62781997a3085d05d29bc05316be56c91c8f3667
Ayoub <ayoubaouinti1990@gmail.com>
2023-05-04 14:19:23 +0000

    forth commit

--- backend-cafe-nodejs/connection.js
+++ backend-cafe-nodejs/connection.js
@@ -8,3 +8,13 @@ let connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
+connection.connect((err)=>{
+    if (!err){
+        console.log('Connected to MySQL');
+    }
+    else{
+        console.log(err);
+    }
+});
+
+module.exports = connection;
--- backend-cafe-nodejs/index.js
+++ backend-cafe-nodejs/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 let cors = require('cors');
-
+const connection = require('./connection');
 const app = express();
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
